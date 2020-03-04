5656a83efec7f128d89bcf6fc0870fbe02251d4f
unknown <x1tmwz@gmail.com>
2020-03-04 23:14:20 +0200

    setup backgammonState

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // let sheshBesh = new Backgammon();
 // document.body.appendChild(sheshBesh.domComponent);
+const backgammonState = new BackgammonState();
 const dice = new Dice();
 const diceUI = new DiceRender();
 const throwDiceButtonHandler =(e)=>{
     dice.throwDice();
+    backgammonState.dispatch({dice:dice.getDice()})
     diceUI.setState(dice.getDice());
+    console.log(backgammonState.getState());
 }
 
 DiceRender.prototype.throwDiceButtonHandler = throwDiceButtonHandler;
